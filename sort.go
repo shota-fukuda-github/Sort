b97543ee30061cf656a643957c4f82618d2e8552
shota <shota.fukuda.leo@gmail.com>
2024-11-17 12:51:12 +0900

    lint

--- sort.go
+++ sort.go
@@ -1,168 +1,165 @@
 package sort
 
-
 import (
-    "slices"
+	"slices"
 )
 
 type Number interface {
-    ~int | ~float64
+	~int | ~float64
 }
 
 type CompareFunc[T Number] func(T, T) bool
 
 // バブルソート
 func BubbleSort[T Number](array []T, compare CompareFunc[T]) []T {
-    for i := range array {
-        for j := i+1; j < len(array); j++ {
-            if compare(array[i], array[j]) {
-                swap(&array[i], &array[j])
-            }
-        }
-    }
-    return array
+	for i := range array {
+		for j := i + 1; j < len(array); j++ {
+			if compare(array[i], array[j]) {
+				swap(&array[i], &array[j])
+			}
+		}
+	}
+	return array
 }
 
 // 選択ソート
 func SelectSort[T Number](array []T, compare CompareFunc[T]) []T {
-    for i := range array {
-        tmp := array[i]
-        idx := i
-        for j:= i+1; j < len(array); j++ {
-            if compare(tmp, array[j]) {
-                tmp = array[j]
-                idx = j
-            }
-        }
-        array[idx] = array[i]
-        array[i]   = tmp
-    }
-    return array
+	for i := range array {
+		tmp := array[i]
+		idx := i
+		for j := i + 1; j < len(array); j++ {
+			if compare(tmp, array[j]) {
+				tmp = array[j]
+				idx = j
+			}
+		}
+		array[idx] = array[i]
+		array[i] = tmp
+	}
+	return array
 }
 
 // クイックソート
 func QuickSort[T Number](array []T, compare CompareFunc[T]) []T {
-    count := len(array)
-    if count <= 1 {
-        return array
-    }
-    // 境界は先頭の要素を使用
-    pivot := array[0]
-    left  := []T{}
-    right := []T{}
-    for i:= 1; i < count; i++ {
-        if compare(pivot, array[i]) {
-            left = append(left, array[i])
-        } else {
-            right = append(right, array[i]) 
-        }
-    }
-    SortedLeft  := QuickSort(left, compare)
-    SortedRight := QuickSort(right, compare)
-    return slices.Concat(append(SortedLeft, pivot), SortedRight)
+	count := len(array)
+	if count <= 1 {
+		return array
+	}
+	// 境界は先頭の要素を使用
+	pivot := array[0]
+	left := []T{}
+	right := []T{}
+	for i := 1; i < count; i++ {
+		if compare(pivot, array[i]) {
+			left = append(left, array[i])
+		} else {
+			right = append(right, array[i])
+		}
+	}
+	SortedLeft := QuickSort(left, compare)
+	SortedRight := QuickSort(right, compare)
+	return slices.Concat(append(SortedLeft, pivot), SortedRight)
 }
-
 
 // マージソート
 func MergeSort[T Number](array []T, compare CompareFunc[T]) []T {
-    count := len(array)
-    if count <= 1 {
-        return array
-    }
-    divNum := count/2
-    left   := array[:divNum]
-    right  := array[divNum:]
+	count := len(array)
+	if count <= 1 {
+		return array
+	}
+	divNum := count / 2
+	left := array[:divNum]
+	right := array[divNum:]
 
-    sortedLeft  := MergeSort(left, compare)
-    sortedRight := MergeSort(right, compare)
-    sorted := make([]T, 0, count)
+	sortedLeft := MergeSort(left, compare)
+	sortedRight := MergeSort(right, compare)
+	sorted := make([]T, 0, count)
 
-    // 右の要素番号保存
-    j := 0
-    for i := range sortedLeft {
-        for ;j < len(sortedRight); j++ {
-            if compare(sortedLeft[i], sortedRight[j]) {
-                sorted = append(sorted, sortedRight[j])
-                // 右の要素が最後だった場合、残る左をすべて入れてreturn
-                if j+1 == len(sortedRight) {
-                    return slices.Concat(sorted, sortedLeft[i:])
-                }
-            } else {
-                sorted = append(sorted, sortedLeft[i])
-                // 左の要素が最後だった場合、残る右をすべて入れてreturn
-                if i+1 == len(sortedLeft) {
-                    return slices.Concat(sorted, sortedRight[j:])
-                }
-                // 次の右の要素も見るのをやめて、左の要素を進める
-                break
-            }
-        }
-    }
-    return sorted
+	// 右の要素番号保存
+	j := 0
+	for i := range sortedLeft {
+		for ; j < len(sortedRight); j++ {
+			if compare(sortedLeft[i], sortedRight[j]) {
+				sorted = append(sorted, sortedRight[j])
+				// 右の要素が最後だった場合、残る左をすべて入れてreturn
+				if j+1 == len(sortedRight) {
+					return slices.Concat(sorted, sortedLeft[i:])
+				}
+			} else {
+				sorted = append(sorted, sortedLeft[i])
+				// 左の要素が最後だった場合、残る右をすべて入れてreturn
+				if i+1 == len(sortedLeft) {
+					return slices.Concat(sorted, sortedRight[j:])
+				}
+				// 次の右の要素も見るのをやめて、左の要素を進める
+				break
+			}
+		}
+	}
+	return sorted
 }
-
 
 // ヒープソート
 func HeapSort[T Number](array []T, compare CompareFunc[T]) []T {
-    for i := range array {
-        current_idx := i
-        for current_idx > 0 {
-            parent_idx := (current_idx -1) / 2
-            // 親子の入れ替わりがあった場合、入れ替え後の親要素を確認する
-            if compare(array[current_idx], array[parent_idx]) {
-                swap(&array[current_idx], &array[parent_idx])
-                current_idx = parent_idx
-            } else {
-                // 親子の入れ替わりがない場合、次の要素を確認する
-                break
-            }
-        }
-    }
+	for i := range array {
+		current_idx := i
+		for current_idx > 0 {
+			parent_idx := (current_idx - 1) / 2
+			// 親子の入れ替わりがあった場合、入れ替え後の親要素を確認する
+			if compare(array[current_idx], array[parent_idx]) {
+				swap(&array[current_idx], &array[parent_idx])
+				current_idx = parent_idx
+			} else {
+				// 親子の入れ替わりがない場合、次の要素を確認する
+				break
+			}
+		}
+	}
 
-    // すでに先頭(最後尾に移動)はソート済みのため、sliceの長さ-1回数分ループ。最後に残った要素もソート不要なので i > 0
-    for i := len(array) - 1; i > 0; i-- {
-        current_idx := 0
-        swap(&array[i], &array[current_idx])
-        left_idx := getLeftIdx(current_idx)
-        // iはすでに確定している要素のインデックスのためそれよりも左の要素のインデックスが小さい場合に入れ替えが発生する
-        // 右の要素のインデックスは左の要素のインデックス+1のため、十分条件は i > left_idx
-        for i > left_idx {
-            right_idx := left_idx + 1;
-            child_idx := left_idx
-            // 右の要素も生存していた場合、左の要素と比べて入れ替える要素を定める
-            if i > right_idx && compare(array[right_idx], array[child_idx]) {
-                child_idx = right_idx
-            }
-            // 親子の入れ替わりがあった場合、入れ替え後の子要素を確認する
-            if compare(array[child_idx], array[current_idx]) {
-                swap(&array[child_idx], &array[current_idx])
-                current_idx = child_idx
-                left_idx = getLeftIdx(current_idx)
-            } else {
-                // 親子の入れ替わりがない場合、次の要素を確認する
-                break
-            }
-        }
-    }
-    return array
+	// すでに先頭(最後尾に移動)はソート済みのため、sliceの長さ-1回数分ループ。最後に残った要素もソート不要なので i > 0
+	for i := len(array) - 1; i > 0; i-- {
+		current_idx := 0
+		swap(&array[i], &array[current_idx])
+		left_idx := getLeftIdx(current_idx)
+		// iはすでに確定している要素のインデックスのためそれよりも左の要素のインデックスが小さい場合に入れ替えが発生する
+		// 右の要素のインデックスは左の要素のインデックス+1のため、十分条件は i > left_idx
+		for i > left_idx {
+			right_idx := left_idx + 1
+			child_idx := left_idx
+			// 右の要素も生存していた場合、左の要素と比べて入れ替える要素を定める
+			if i > right_idx && compare(array[right_idx], array[child_idx]) {
+				child_idx = right_idx
+			}
+			// 親子の入れ替わりがあった場合、入れ替え後の子要素を確認する
+			if compare(array[child_idx], array[current_idx]) {
+				swap(&array[child_idx], &array[current_idx])
+				current_idx = child_idx
+				left_idx = getLeftIdx(current_idx)
+			} else {
+				// 親子の入れ替わりがない場合、次の要素を確認する
+				break
+			}
+		}
+	}
+	return array
 }
 
 // 昇順用の関数
 func Desc[T Number](num1, num2 T) bool {
-    return num1 < num2
+	return num1 < num2
 }
 
 // 降順用の関数
 func Asc[T Number](num1, num2 T) bool {
-    return num1 > num2
+	return num1 > num2
 }
 
 // 値の入れ替えを行う
 func swap[T Number](num1, num2 *T) {
-    *num1, *num2 = *num2, *num1
+	*num1, *num2 = *num2, *num1
 }
 
 // ヒープソート用。引数の左の要素のインデックスを返す
 func getLeftIdx(current_idx int) int {
-    return 2 * current_idx + 1
+	return 2*current_idx + 1
 }
--- sort_test.go
+++ sort_test.go
@@ -1,209 +1,208 @@
 package sort
 
 import (
-	"testing"
 	"math/rand"
+	"testing"
 )
 
 type TestStruct[T Number] struct {
-	name string
+	name  string
 	array []T
-	sort func(T, T) bool
-	want []T
+	sort  func(T, T) bool
+	want  []T
 }
 
 func TestIntBubbleSort(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 	tests := getCommonIntSortTestStruct()
 
 	for _, tt := range tests {
-        tt := tt
-        t.Run(tt.name, func(t *testing.T) {
-            t.Parallel()
-            got := BubbleSort(tt.array, tt.sort)
-            if !isEqualSlice(got, tt.want) {
-                t.Errorf("BubbleSort(%v) == %v, want %v", tt.array, got, tt.want)
-            }
-        })
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := BubbleSort(tt.array, tt.sort)
+			if !isEqualSlice(got, tt.want) {
+				t.Errorf("BubbleSort(%v) == %v, want %v", tt.array, got, tt.want)
+			}
+		})
 	}
 }
 
 func TestFolatBubbleSort(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 	tests := getCommonFloatSortTestStruct()
 
 	for _, tt := range tests {
-        tt := tt
-        t.Run(tt.name, func(t *testing.T) {
-            t.Parallel()
-            got := BubbleSort(tt.array, tt.sort)
-            if !isEqualSlice(got, tt.want) {
-                t.Errorf("BubbleSort(%v) == %v, want %v", tt.array, got, tt.want)
-            }
-        })
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := BubbleSort(tt.array, tt.sort)
+			if !isEqualSlice(got, tt.want) {
+				t.Errorf("BubbleSort(%v) == %v, want %v", tt.array, got, tt.want)
+			}
+		})
 	}
 }
 
 func TestIntSelectSort(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 	tests := getCommonIntSortTestStruct()
 
 	for _, tt := range tests {
-        tt := tt
-        t.Run(tt.name, func(t *testing.T) {
-            t.Parallel()
-            got := SelectSort(tt.array, tt.sort)
-            if !isEqualSlice(got, tt.want) {
-                t.Errorf("SelectSort(%v) == %v, want %v", tt.array, got, tt.want)
-            }
-        })
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := SelectSort(tt.array, tt.sort)
+			if !isEqualSlice(got, tt.want) {
+				t.Errorf("SelectSort(%v) == %v, want %v", tt.array, got, tt.want)
+			}
+		})
 	}
 }
 
 func TestFloatSelectSort(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 	tests := getCommonFloatSortTestStruct()
 
 	for _, tt := range tests {
-        tt := tt
-        t.Run(tt.name, func(t *testing.T) {
-            t.Parallel()
-            got := SelectSort(tt.array, tt.sort)
-            if !isEqualSlice(got, tt.want) {
-                t.Errorf("SelectSort(%v) == %v, want %v", tt.array, got, tt.want)
-            }
-        })
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := SelectSort(tt.array, tt.sort)
+			if !isEqualSlice(got, tt.want) {
+				t.Errorf("SelectSort(%v) == %v, want %v", tt.array, got, tt.want)
+			}
+		})
 	}
 }
 
-
 func TestIntQuickSort(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 	tests := getCommonIntSortTestStruct()
 
 	for _, tt := range tests {
-        tt := tt
-        t.Run(tt.name, func(t *testing.T) {
-            t.Parallel()
-            got := QuickSort(tt.array, tt.sort)
-            if !isEqualSlice(got, tt.want) {
-                t.Errorf("QuickSort(%v) == %v, want %v", tt.array, got, tt.want)
-            }
-        })
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := QuickSort(tt.array, tt.sort)
+			if !isEqualSlice(got, tt.want) {
+				t.Errorf("QuickSort(%v) == %v, want %v", tt.array, got, tt.want)
+			}
+		})
 	}
 }
 
 func TestFloatQuickSort(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 	tests := getCommonFloatSortTestStruct()
 
 	for _, tt := range tests {
-        tt := tt
-        t.Run(tt.name, func(t *testing.T) {
-            t.Parallel()
-            got := QuickSort(tt.array, tt.sort)
-            if !isEqualSlice(got, tt.want) {
-                t.Errorf("QuickSort(%v) == %v, want %v", tt.array, got, tt.want)
-            }
-        })
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := QuickSort(tt.array, tt.sort)
+			if !isEqualSlice(got, tt.want) {
+				t.Errorf("QuickSort(%v) == %v, want %v", tt.array, got, tt.want)
+			}
+		})
 	}
 }
 
 func TestIntMergeSort(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 	tests := getCommonIntSortTestStruct()
 
 	for _, tt := range tests {
-        tt := tt
-        t.Run(tt.name, func(t *testing.T) {
-            t.Parallel()
-            got := MergeSort(tt.array, tt.sort)
-            if !isEqualSlice(got, tt.want) {
-                t.Errorf("MergeSort(%v) == %v, want %v", tt.array, got, tt.want)
-            }
-        })
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := MergeSort(tt.array, tt.sort)
+			if !isEqualSlice(got, tt.want) {
+				t.Errorf("MergeSort(%v) == %v, want %v", tt.array, got, tt.want)
+			}
+		})
 	}
 }
 
 func TestFloatMergeSort(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 	tests := getCommonFloatSortTestStruct()
 
 	for _, tt := range tests {
-        tt := tt
-        t.Run(tt.name, func(t *testing.T) {
-            t.Parallel()
-            got := MergeSort(tt.array, tt.sort)
-            if !isEqualSlice(got, tt.want) {
-                t.Errorf("MergeSort(%v) == %v, want %v", tt.array, got, tt.want)
-            }
-        })
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := MergeSort(tt.array, tt.sort)
+			if !isEqualSlice(got, tt.want) {
+				t.Errorf("MergeSort(%v) == %v, want %v", tt.array, got, tt.want)
+			}
+		})
 	}
 }
 
 func TestIntHeapSort(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 	tests := getCommonIntSortTestStruct()
 
 	for _, tt := range tests {
-        tt := tt
-        t.Run(tt.name, func(t *testing.T) {
-            t.Parallel()
-            got := HeapSort(tt.array, tt.sort)
-            if !isEqualSlice(got, tt.want) {
-                t.Errorf("HeapSort(%v) == %v, want %v", tt.array, got, tt.want)
-            }
-        })
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := HeapSort(tt.array, tt.sort)
+			if !isEqualSlice(got, tt.want) {
+				t.Errorf("HeapSort(%v) == %v, want %v", tt.array, got, tt.want)
+			}
+		})
 	}
 }
 
 func TestFloatHeapSort(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 	tests := getCommonFloatSortTestStruct()
 
 	for _, tt := range tests {
-        tt := tt
-        t.Run(tt.name, func(t *testing.T) {
-            t.Parallel()
-            got := HeapSort(tt.array, tt.sort)
-            if !isEqualSlice(got, tt.want) {
-                t.Errorf("HeapSort(%v) == %v, want %v", tt.array, got, tt.want)
-            }
-        })
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := HeapSort(tt.array, tt.sort)
+			if !isEqualSlice(got, tt.want) {
+				t.Errorf("HeapSort(%v) == %v, want %v", tt.array, got, tt.want)
+			}
+		})
 	}
 }
 
-func getCommonIntSortTestStruct() []TestStruct[int]{
-	return []TestStruct[int] {
+func getCommonIntSortTestStruct() []TestStruct[int] {
+	return []TestStruct[int]{
 		{"配列の要素0: ASC", []int{}, Asc[int], []int{}},
 		{"配列の要素1: ASC", []int{1}, Asc[int], []int{1}},
-		{"配列の要素2: ASC", []int{1,2}, Asc[int], []int{1,2}},
-		{"配列の要素3: ASC", []int{3,2,1}, Asc[int], []int{1,2,3}},
-		{"同一値あり : ASC", []int{3,2,3}, Asc[int], []int{2,3,3}},
-		{"マイナス値 : ASC", []int{-3,0,-5,1}, Asc[int], []int{-5,-3,0,1}},
+		{"配列の要素2: ASC", []int{1, 2}, Asc[int], []int{1, 2}},
+		{"配列の要素3: ASC", []int{3, 2, 1}, Asc[int], []int{1, 2, 3}},
+		{"同一値あり : ASC", []int{3, 2, 3}, Asc[int], []int{2, 3, 3}},
+		{"マイナス値 : ASC", []int{-3, 0, -5, 1}, Asc[int], []int{-5, -3, 0, 1}},
 		{"配列の要素0: DESC", []int{}, Desc[int], []int{}},
 		{"配列の要素1: DESC", []int{2}, Desc[int], []int{2}},
-		{"配列の要素2: DESC", []int{1,2}, Desc[int], []int{2,1}},
-		{"配列の要素3: DESC", []int{3,2,1}, Desc[int], []int{3,2,1}},
-		{"同一値あり : DESC", []int{3,2,3}, Desc[int], []int{3,3,2}},
-		{"マイナス値 : DESC", []int{-3,0,-5,1}, Desc[int], []int{1,0,-3,-5}},
+		{"配列の要素2: DESC", []int{1, 2}, Desc[int], []int{2, 1}},
+		{"配列の要素3: DESC", []int{3, 2, 1}, Desc[int], []int{3, 2, 1}},
+		{"同一値あり : DESC", []int{3, 2, 3}, Desc[int], []int{3, 3, 2}},
+		{"マイナス値 : DESC", []int{-3, 0, -5, 1}, Desc[int], []int{1, 0, -3, -5}},
 	}
 }
 
-func getCommonFloatSortTestStruct() []TestStruct[float64]{
-	return []TestStruct[float64] {
+func getCommonFloatSortTestStruct() []TestStruct[float64] {
+	return []TestStruct[float64]{
 		{"配列の要素0: ASC", []float64{}, Asc[float64], []float64{}},
 		{"配列の要素1: ASC", []float64{1.0}, Asc[float64], []float64{1.0}},
-		{"配列の要素2: ASC", []float64{1.1,2.1}, Asc[float64], []float64{1.1,2.1}},
-		{"配列の要素3: ASC", []float64{3,2,1}, Asc[float64], []float64{1,2,3}},
-		{"同一値あり : ASC", []float64{3.3,2.33,3.3}, Asc[float64], []float64{2.33,3.3,3.3}},
-		{"マイナス値 : ASC", []float64{-3.22,0.22,-5.22,1.22}, Asc[float64], []float64{-5.22,-3.22,0.22,1.22}},
+		{"配列の要素2: ASC", []float64{1.1, 2.1}, Asc[float64], []float64{1.1, 2.1}},
+		{"配列の要素3: ASC", []float64{3, 2, 1}, Asc[float64], []float64{1, 2, 3}},
+		{"同一値あり : ASC", []float64{3.3, 2.33, 3.3}, Asc[float64], []float64{2.33, 3.3, 3.3}},
+		{"マイナス値 : ASC", []float64{-3.22, 0.22, -5.22, 1.22}, Asc[float64], []float64{-5.22, -3.22, 0.22, 1.22}},
 		{"配列の要素0: DESC", []float64{}, Desc[float64], []float64{}},
 		{"配列の要素1: DESC", []float64{2.44}, Desc[float64], []float64{2.44}},
-		{"配列の要素2: DESC", []float64{1.1234,2.1234}, Desc[float64], []float64{2.1234,1.1234}},
-		{"配列の要素3: DESC", []float64{3.123,2.123,1.123}, Desc[float64], []float64{3.123,2.123,1.123}},
-		{"同一値あり : DESC", []float64{3.123,2.123,3.123}, Desc[float64], []float64{3.123,3.123,2.123}},
-		{"マイナス値 : DESC", []float64{-3.4321,0.43,-5.1,1.123}, Desc[float64], []float64{1.123,0.43,-3.4321,-5.1}},
+		{"配列の要素2: DESC", []float64{1.1234, 2.1234}, Desc[float64], []float64{2.1234, 1.1234}},
+		{"配列の要素3: DESC", []float64{3.123, 2.123, 1.123}, Desc[float64], []float64{3.123, 2.123, 1.123}},
+		{"同一値あり : DESC", []float64{3.123, 2.123, 3.123}, Desc[float64], []float64{3.123, 3.123, 2.123}},
+		{"マイナス値 : DESC", []float64{-3.4321, 0.43, -5.1, 1.123}, Desc[float64], []float64{1.123, 0.43, -3.4321, -5.1}},
 	}
 }
 
@@ -219,7 +218,6 @@ func isEqualSlice[T comparable](arrayA, arrayB []T) bool {
 	}
 	return true
 }
-
 
 func BenchmarkBubbleSort(b *testing.B) {
 	b.ResetTimer()
@@ -261,7 +259,7 @@ func BenchmarkHeapSort(b *testing.B) {
 	}
 }
 
-func makeRandomSlice(n int) []float64{
+func makeRandomSlice(n int) []float64 {
 	array := make([]float64, n)
 	for range n {
 		array = append(array, rand.Float64())
